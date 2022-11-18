8e0967bec1f91467c9ce196f88c20fef76e120ed
Pawel Paczkowski <paw.pac@yahoo.com>
2022-11-17 21:17:00 -0600

    Test "create react app"

---
+++ react-app-test/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './components/Test';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <>
+          <h1>Test React</h1>
+          <Test />
+        </>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app-test/src/components/Test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <h2>Test component</h2>
+  );
+};
+
+export default Test;
